3646ef16d7d2c560a09fc042c21d7391ebc52128
AlexandrNachmanovich <a.nachmanovich@gmail.com>
2025-08-03 17:59:48 +0300

    Настроил base и переместил картинки

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './',
+  base: '/journal-memory/',
+
   plugins: [
     react(),
     VitePWA({
